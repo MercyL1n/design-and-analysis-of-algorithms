45b5cae2b0110a99a952d40edb93c21f7f7bc5af
MercyLin <18506000485@163.com>
2019-10-29 09:13:20 +0800

    添加了凸多边形最优三角剖分

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -79,5 +79,27 @@ public class DynamicProgramming {
         }else if(b[i][j] == 2) lcs(i - 1, j, x, b);
             else lcs(i, j - 1, x, b);
     }
+
+    public static int w(int i, int k, int j){
+        //计算权值
+        return 0;
+    }
+    public static void minWeightTriangulation(int n, int[][] t, int[][] s){
+        for (int i = 1; i <= n; i++) t[i][i] = 0;
+        for (int r = 2; r <= n; r++)
+            for (int i = 1; i < n - r + 1; i++) {
+                int j = i + r - 1;
+                t[i][j] = t[i + 1][j] + w(i - 1, i, j);
+                s[i][j] = i;
+                for (int k = i + 1; k < j; k++) {
+                    //以vi-1,vj为底的三角形的变化
+                    int u = t[i][k] + t[k + 1][j] + w(i - 1, k, j);
+                    if(u < t[i][j]){
+                        t[i][j] = u;
+                        s[i][j] = k;
+                    }
+                }
+            }
+    }
 }
 
